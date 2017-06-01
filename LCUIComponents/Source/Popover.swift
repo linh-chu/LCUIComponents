5a5a1ee15217795046feb119003e357e069fd9a1
Linh Chu <linhcn@philology.com.au>
2017-06-01 10:06:03 +1000

    restructure the files

--- LCUIComponents/Source/Popover.swift
+++ LCUIComponents/Source/Popover.swift
@@ -11,22 +11,24 @@ import UIKit
 
 typealias KeyValueTuple = (key: Int, value: String)
 
-protocol PopupDialogDelegate: class {
+protocol LCPopoverDelegate: class {
     func didSelectData(_ selectedData: KeyValueTuple)
 }
 
-open class PopupDialog: UIViewController {
+open class LCPopover: UIViewController {
     
     let cellIdentifier = "Cell"
     
-    weak var delegate: BasePopoverDelegate?
+    weak var delegate: LCPopoverDelegate?
     var tableView: UITableView!
     var arrayData = [KeyValueTuple]()
     
     convenience init(for sender: UIView, _ size: CGSize = CGSize(width: 250, height: 230)) {
         self.init()
         modalPresentationStyle = .popover
-        preferredContentSize = size
+        preferredContentSize = size        
+        self.view.frame = CGRect(origin: CGPoint(x: 0, y: 0), size: size)
+        
         updatePosition(for: sender)
     }
     
@@ -39,11 +41,37 @@ open class PopupDialog: UIViewController {
         }
     }
     
-    override func viewDidLoad() {
+    override open func viewDidLoad() {
         super.viewDidLoad()
         
-        tableView = UITableView()
-        
+        let barHeight = UIApplication.shared.statusBarFrame.size.height
+        let tableWidth = preferredContentSize.width
+        let tableHeight = preferredContentSize.height
+        let tableFrame = CGRect(x: 0, y: barHeight, width: tableWidth, height: tableHeight - barHeight)
+        tableView = UITableView(frame: tableFrame, style: .grouped)
+        tableView.dataSource = self
+        tableView.delegate = self
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
+        self.view.addSubview(tableView)
     }
 }
+
+extension LCPopover: UITableViewDelegate, UITableViewDataSource {
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arrayData.count
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier) as UITableViewCell!
+
+        cell.textLabel?.text = arrayData[indexPath.row].value
+        return cell
+    }
+    
+    public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        delegate?.didSelectData(arrayData[indexPath.row])
+        dismiss(animated: true, completion: nil)
+    }
+}
+
